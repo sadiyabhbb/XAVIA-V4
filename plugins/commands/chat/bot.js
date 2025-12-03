572805ec972a81c53093acf203b4499a9eb560ba
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-03 17:57:09 +0600

    Update bot.js

--- plugins/commands/chat/bot.js
+++ plugins/commands/chat/bot.js
@@ -16,15 +16,16 @@ const SIM_API_URL = "http://65.109.80.126:20392/sim";
 
 function ensureCache() {
   const defaultData = [
-    "Hello! How can I help you today?",
-    "I'm always here for you!",
-    "What's up? 😊",
-    "Need any help?",
-    "Hi there! I'm your bot buddy.",
-    "Bot is alive 😎",
-    "Ready to respond anytime!",
-    "How’s your day going?"
-  ];
+    "আহ শুনা আমার তোমার অলিতে গলিতে উম্মাহ😇😘",
+          "কি গো সোনা আমাকে ডাকছ কেনো",
+          "বার বার আমাকে ডাকস কেন😡",
+          "আহ শোনা আমার আমাকে এতো ডাক্তাছো কেনো আসো বুকে আশো🥱",
+          "হুম জান তোমার অইখানে উম্মমাহ😷😘",
+          "আসসালামু আলাইকুম বলেন আপনার জন্য কি করতে পারি",
+          "আমাকে এতো না ডেকে বস নয়নকে একটা গফ দে 🙄",
+          "jang hanga korba",
+          "jang bal falaba🙂"
+        ];
 
   if (!fs.existsSync("./cache")) fs.mkdirSync("./cache");
 
